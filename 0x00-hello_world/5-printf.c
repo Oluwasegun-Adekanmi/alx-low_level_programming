ea4ec1be1398e2b05f318560e3b7e28b6f29755f
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-16 08:06:06 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 int main(void)
 	/*program starts here*/
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
