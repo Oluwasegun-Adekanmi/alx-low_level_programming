92a7028e93c78fe16cb3e74ff47fc633ddba18eb
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-16 10:31:22 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <unistd.h>
+/**
+ * main - prints exactly and that piece of art is useful
+ *
+ * Return: Always 1 (succes)
+ *
+*/
+int main(void)
+	/*program goes here*/
+{
+	write(2, "and that piece of art is useful"\- Dora Korpar, 2015-10-19, 59);
+	return (1);	
+}
