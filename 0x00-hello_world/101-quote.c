793b7b5608e85cfa5d2e668670274c4c1222e8eb
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-16 14:02:12 -0700

    Task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 # include <stdio.h>
 # include <unistd.h>
 /**
- * main - prints exactly and that piece of art is useful
+ *
+ * main - Program to print certain words without using printf
  *
  * Return: Always 1 (succes)
  *
 */
 int main(void)
 
-	/*program goes here*/
+	/* Program goes right here*/
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
