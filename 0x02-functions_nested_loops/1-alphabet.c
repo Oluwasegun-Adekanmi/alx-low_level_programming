b0c4adf574c2a15fe36e27f9f877f2fbbd18ebad
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-19 18:18:14 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0 (success)
+ *
+*/
+
+void print_alphabet(void);
+{
+	char lowcase;
+
+	for
+	       (lowcase = 'a');
+	       (lowcase <= 'z');
+		lowcase++;
+
+	{
+		_putchar(lowcase);
+	}
+		_putchar('\n');
+
+	return (0);
+}
