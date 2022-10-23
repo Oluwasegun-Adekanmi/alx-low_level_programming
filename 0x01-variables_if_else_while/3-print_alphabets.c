d0aeb3b6097667e0d6cd29a10f6216b38fe600e3
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-23 09:50:44 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point into the program
+ *
+ * Return: Always 0 (success)
+ *
+**/
+
+int main(void)
+{
+	char i;
+	char r;
+
+	for
+		(i = 'a'; i <= 'z'; i++);
+	{
+		putchar(i);
+	}
+
+	for
+		(r = 'A'; r <= 'Z'; r++);
+	{
+		putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
