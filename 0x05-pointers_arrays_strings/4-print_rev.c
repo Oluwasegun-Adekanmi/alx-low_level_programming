3f90d3093d79aef43475f949980786908e88fd50
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-25 03:29:39 -0700

    C - Pointers, arrays and strings C

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@ void print_rev(char *s)
 
 	while (s[count] != '\0')
 		count++;
-	for (count = count - 1; count >= 0; count++)
+	for (count = count - 1; count >= 0; count--)
 		_putchar(s[count]);
 
 	_putchar('\n');
