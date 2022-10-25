206a98844f34d76ed3f861caaf0542cee6b28915
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-25 00:01:55 -0700

    C - Pointers, arrays and strings C

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * swap_int - Swap the value of two integers.
- * @*a:First value to be swapped
- * @*b:Second value to be swapped
+ * @a:First argument to be swapped
+ * @b:Second argument to be swapped
  * Return: Always void.
 */
 
 void swap_int(int *a, int *b)
-
 {
 	int swap;
+
 	swap = *a;
 	*a = *b;
 	*b = swap;
