dcf42060e9db4cbc8b02e244ab5be6ac5c599c6f
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-26 05:03:17 -0700

    0X06 - More Pointers and Arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,9 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; i <= (*(src); i++);
-		
-		{
-			(*(dest + i)) = (*(src[i]));
-			_putchar("%s" (*(dest));
-		}
+	for (i = 0; i <= (*(src); i++)
+	{
+		(*(dest + i)) = (*(src[i]));
+		_putchar("%s", *(dest));
+	}
 }
-			
