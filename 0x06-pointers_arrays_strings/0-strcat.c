920837718c1b4478040e9158b4391af3c2492173
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-26 04:39:02 -0700

    )

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - appends the src string to the dest string
+ * @src: Source parameter
+ * @dest: Destination parameter
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= (*(src); i++);
+		
+		{
+			(*(dest + i)) = (*(src[i]));
+			_putchar("%s" (*(dest));
+		}
+}
+			
