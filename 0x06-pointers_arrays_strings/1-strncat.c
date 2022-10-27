6382548ae96563705c70eaef3e3fba1ae7419920
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-27 10:19:36 -0700

    0X06 - More Pointers and Arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,22 +9,23 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int ctrl1;
-        int ctrl2;
+	int ctrl2;
 
-        ctrl1 = 0;
-        while (dest[ctrl1] != '\0')
-        {
-                ctrl1++;
-        }
+	ctrl1 = 0;
 
-        ctrl2 = 0;
-        while (ctrl2 < n && src[ctrl2] != '\0')
-        {
-                dest[ctrl1] = src[ctrl2];
-                ctrl1++;
-                ctrl2++;
-        }
-        dest[ctrl1] = '\0';
+	while (dest[ctrl1] != '\0')
+	{
+		ctrl1++;
+	}
 
-        return (dest);
+	ctrl2 = 0;
+
+	while (ctrl2 < n && src[ctrl2] != '\0')
+	{
+		dest[ctrl1] = src[ctrl2];
+		ctrl1++;
+		ctrl2++;
+	}
+	dest[ctrl1] = '\0';
+	return (dest);
 }
