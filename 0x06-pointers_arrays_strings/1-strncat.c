c11cb8e0fb9fe0b2362e6c2504783a0e04f07a57
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-27 10:05:21 -0700

    0X06 - More Pointers and Arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,25 +6,25 @@
  * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int)
 {
 	int ctrl1;
-	int ctrl2;
+        int ctrl2;
 
-	ctrl1 = 0;
-	while (dest[ctrl1] != '\0')
-	{
-		ctrl1++;
-	}
+        ctrl1 = 0;
+        while (dest[ctrl1] != '\0')
+        {
+                ctrl1++;
+        }
 
-	ctrl2 = 0;
-	while (ctrl2 < n && src[ctrl2] != '\0')
-	{
-		dest[ctrl1] = src[ctrl2];
-		ctrl1++;
-		ctrl2++;
-	}
-	dest[ctrl1] = '\0';
+        ctrl2 = 0;
+        while (ctrl2 < n && src[ctrl2] != '\0')
+        {
+                dest[ctrl1] = src[ctrl2];
+                ctrl1++;
+                ctrl2++;
+        }
+        dest[ctrl1] = '\0';
 
-	return (dest);
+        return (dest);
 }
