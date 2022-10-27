8b871267577b4150aa7f3480eebae1360af91b43
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-27 11:41:30 -0700

    0X06 - More Pointers and Arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,15 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int control;
 
-	for (control = 0; s1[control] != '\0'; control++;)
+	for (control = 0; s1[control] != '\0' || s2[control] != '\0'; control++)
 	{
-
-		if (s1[control] == s2[control]);
-
+		if (s1[control] != s2[control])
 		{
-			return (s1[control] = s2[control]);
+			return (s1[control] - s2[control]);
 		}
-
-		return (0);
 	}
+	return (0);
 }
