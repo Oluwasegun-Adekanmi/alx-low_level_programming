35c62d617bdde28ad1891b5d6cd4f5b15fad36d3
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-27 11:34:03 -0700

    0X06 - More Pointers and Arrays

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Always (0)
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int control;
+
+	for (control = 0; s1[control] != '\0'; control++;)
+	{
+
+		if
+			(s1[control] == s2[control]);
+
+		{
+			return (s1[control] = s2[control]);
+		}
+
+		return (0);
+	}
+}
