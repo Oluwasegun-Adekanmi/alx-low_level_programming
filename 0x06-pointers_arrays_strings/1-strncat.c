90895d9ff2251360e9f622c1763932de1415d5d6
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-27 10:07:35 -0700

    0X06 - More Pointers and Arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-char *_strncat(char *dest, char *src, int)
+char *_strncat(char *dest, char *src, int n)
 {
 	int ctrl1;
         int ctrl2;
