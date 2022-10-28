6939f551f0e9d94ed19c63fe32bc96653fccc4a9
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-27 18:04:49 -0700

    0X06 - More Pointers and Arrays

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,30 +1,25 @@
 #include "main.h"
 /**
- * cap_string - capitalizes all words of a string
+ * string_toupper - capitalizes all words of a string
  * @s: input string.
  * Return: the pointer to dest.
  */
 
-char *cap_string(char *s)
+char *string_toupper(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	int control;
+
+	for (control = 0; s[control] != '\0'; control++)
 	{
-		for (i = 0; i < 13; i++)
+		if (s[control] >= 'A' && s[control] <= 'Z')
 		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
+
+			s[control] = s[control] + 32;
+
 		}
-		count++;
+
+		return (s);
+
 	}
-	return (s);
 }
