f95f3c440443adefc669dab53270458efbe3d007
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-10-29 05:12:37 -0700

    0X06 - More Pointers and Arrays

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
- * string_toupper - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
+ * *string_toupper - converts lower case to upper case
+ * @str: strings to be converted
+ * Return: return 0 for success
+ *
  */
 
 char *string_toupper(char *str)
 {
 	int count = 0;
 
-	while (str[count] != '\0')
+	while (str[count])
 	{
-		if (str[count] >= 'A' && str[count] <= 'Z')
-			str[count] +- 32;
+		if (str[count] >= 'a' && str[count] <= 'z')
+			str[count] -= 32;
+
 		count++;
 	}
 
