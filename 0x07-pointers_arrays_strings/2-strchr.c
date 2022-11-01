efeae7417cf8a76b5e06037e7e5ae9c9f5d7bbd8
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-11-01 09:10:15 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,14 +6,26 @@
  *Return: Null
  */
 
+//char *_strchr(char *s, char c)
+//{
+//	while (*s != '\0')
+//	{
+//		if (*s == c)
+//			return ((char *) s);
+//		s++;
+//	}
+//
+//	return ('\0');
+//}
+
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return ((char *) s);
-		s++;
-	}
+	unsigned int i = 0;
 
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
 	return ('\0');
 }
