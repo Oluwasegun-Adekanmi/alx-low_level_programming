ed47d99161ba7174d633a9e7331e5d8e58efb393
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-11-01 19:05:13 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: second string
+ * Return: Null
+ *
+ */
+
+unsigned int _strbrk(char *s, char *accept)
+{
+	unsigned int count, count1;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		for (count1 = 0; accept[count1] != '\0'; count1++)
+		{
+			if (s[count] == accept[count1])
+				return (s[count]);
+		}
+	}
+
+	return ('\0');
+}
