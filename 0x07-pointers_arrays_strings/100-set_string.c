6551ed7c303547a89fef4009a50676f7e085f7ce
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-11-03 03:38:28 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/**
- * set_string - sets the valuw of a pointer to char
- * @s: source pointer
- * @to: destination pointer
- *
- */
 
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;
