50dc3426494160872c75e3dcd9c859bf81970228
Oluwaegun-Adekanmi <oluwasegunojoileri@gmail.com>
2022-11-05 23:51:10 -0700

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	_putchar('\n');
+	if (*s)
 	{
-		_putchar('\n');
-		return;
-	}
 	_putchar(*s);
-	s++;
-
-	_puts_recursion(s);
+	_puts_recursion(s + 1);
+	}
 }
